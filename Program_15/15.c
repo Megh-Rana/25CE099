003d6c626ee29baf1ad296a2432c4735117d604b
Megh Rana <meghthedev@gmail.com>
2025-08-19 09:54:15 +0530

    practicals: 15: solve supplementary problems

--- Program_15/15.c
+++ Program_15/15.c
@@ -1,18 +1,46 @@
 #include <stdio.h>
-
+#include <unistd.h>
 int main()
 {
-    int seconds;
-    printf("Welcome to timer. enter the time you want in seconds\n");
-    scanf("%d",&seconds);
-    while (seconds > 0)
+    int time_input,minutes,seconds,choice;
+    input:
+    printf("\nPlease enter number of seconds for countdown: ");
+    scanf("%d",&time_input);
+    minutes = time_input / 60;
+    seconds = time_input % 60;
+    if(time_input>0)
     {
-        printf("Time Remaining: %d\n",seconds);
-        sleep(1);
-        seconds--;
-    }
-    printf("Time Up!\n");
-    printf("Name: Megh Rana\nBranch: CE CSPIT\nStudent ID: 25TCE3FA\n");
+        while(1)
+        {
+            printf("%d minute(s) :%d second(s)\n",minutes,seconds);
+            sleep(1);
+            if(seconds==0 && minutes!=0)
+            {
+                minutes--;
+                seconds=60;
+            }
+            seconds--;
+            if(minutes==0 && seconds==0)
+            {
+                break; 
+            }
+        }
+        printf("Countdown Completed!\n");
 
-    return 0;
+    }
+    else
+    {
+        printf("Invalid Input!");
+        goto input;
+    }
+    printf("Do you want another countdown? 1 for yes, 0 for no\n");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+        goto input;
+    }
+    else
+    {
+        return 0;
+    }
 }
